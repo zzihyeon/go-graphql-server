4a1b24d6781e5840defe90788093bb46ccf267e1
zzihyeon <guswldb12@naver.com>
2021-01-31 16:19:43 +0900

    fix: add mongo

--- server.go
+++ server.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/zzihyeon/go-graphql-server/controller/graphql"
+	"github.com/zzihyeon/go-graphql-server/infrastructure/db"
 )
 
 func main() {
 	defaultGraphqlPort := "8080"
 	// defaultRestPort := "8080"
 	go graphql.Setup(defaultGraphqlPort)
+	go db.Setup()
 	// go rest.Setup(defaultRestPort)
 	for {
 	}
