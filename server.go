8eca1ccb2daa453925a03945b5d32b6e3dd77949
zzihyeon <guswldb12@naver.com>
2021-04-18 23:39:38 +0900

    feat: add initialize

--- server.go
+++ server.go
@@ -1,7 +1,19 @@
 package main
 
-func main() {
+import (
+	"go-graphql-server/controller"
+	"go-graphql-server/infrastructure"
+	"go-graphql-server/service"
+)
 
+func main() {
+	initialize()
 	for {
 	}
+}
+
+func initialize() {
+	controller.Initialize()
+	service.Initialize()
+	infrastructure.Initialize()
 }
